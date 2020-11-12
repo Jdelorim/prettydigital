8c3638e6e75df7d12bf0df44d7a552c1003e7790
Joshua <joshuadelorimier@gmail.com>
2020-11-12 14:32:48 -0500

    e

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,35 @@
 'use strict'
-
-console.log('hello josh');
+if(window.location.pathname === '/prettydigital') {
+console.log('sup dawg');
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer( {antialias: true} );
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
 const onWindowResize = () => {
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 }
+
 window.addEventListener( 'resize', onWindowResize, false );
 
-var geo = new THREE.BoxBufferGeometry(4,4,4,1,1,1);
-var mat = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-var cube = new THREE.Mesh(geo, mat);
-var light = new THREE.DirectionalLight('rgb(255,255,255)', 1, 50);
-light.position.set(2,0,0);
-scene.add(light);
-scene.add(cube);
+const geo = new THREE.BoxGeometry(4,4,4,1,1,1);
+const mat = new THREE.MeshPhongMaterial( {color: 0x00ff00} );
+const cube = new THREE.Mesh(geo, mat);
+const light = new THREE.PointLight('rgb(255,255,255)', 1, 50);
 
+light.position.set(-5,0,0);
+
+cube.position.z = -10.0;
+cube.position.x = 0.0;
+cube.position.y = 0.0;
+
+scene.add(cube);
+scene.add(light);
 
 const render = () => {
     renderer.render(scene, camera);
@@ -35,3 +41,5 @@ const GameLoop = () => {
 }
 
 
+    GameLoop();
+}
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@ module.exports = (app) => {
     app.get("/", (req, res) => {
         res.render("index", {title: "Pretty Digital"});
     });
+
+    app.get("/prettydigital", (req, res) => {
+        res.render("prettydigital", {title: "music video"});
+    });
 }
